7e044ea524db76090364b080315e67f8573ccef2
Toby Griffiths <toby@cubicmushroom.co.uk>
2013-06-25 10:48:42 +0100

    Initial work on plugins classes to extend wordpress-core
    Migrating initial work completed as part of NCF Fund & Application plugin development.
    
    Extends the cubicmushroom/wordpress-core library

---
+++ src/CubicMushroom/WP/Plugins/Plugin.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Base Plugin class to be extended by individual plugins
+ *
+ * PHP version 5
+ * 
+ * @category   WordPress_Plugins
+ * @package    CubicMushroom_WP
+ * @subpackage Plugins
+ * @author     Toby Griffiths <toby@cubicmushroom.co.uk>
+ * @license    http://opensource.org/licenses/MIT MIT
+ * @link       http://cubicmushroom.co.uk
+ **/
+
+namespace CubicMushroom\WP\Plugins;
+
+use \CubicMushroom\WP\Core\Base;
+use \CubicMushroom\WP\Exception\PostTypeRegistrationFailedException;
+
+/**
+ * NCF Fund & Application WordPress plugin core class
+ * 
+ * @category   WordPress_Plugins
+ * @package    CubicMushroom_WP
+ * @subpackage Plugins
+ * @author     Toby Griffiths <toby@cubicmushroom.co.uk>
+ * @license    http://opensource.org/licenses/MIT MIT
+ * @link       http://cubicmushroom.co.uk
+ **/
+class Plugin extends Base
+{
+
+    /*************************
+     * Non-static properties *
+     *************************/
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $hooks = array(
+        'plugins_loaded' => 'hookSetup',
+        'init'           => 'hookInit',
+        'admin_init'     => 'hookAdminInit'
+    );
+
+} // END class FundApplicationPlugin
